63a0972d4f88ccf864cca73913c8882bde9eb9b9
SUBHRANIL GHOSH <subhranil.blue07@gmail.com>
2026-08-05 23:43:42 +0530

    third commit

---
+++ src/langgraphagenticai/state/state.py
@@ -0,0 +1,13 @@
+from typing_extensions import TypedDict,list
+from langgaph.graph.message import add_messages
+from typing import Annotated
+
+class State(TypedDict):
+    """
+    Represent the structure of the state used in the graph.
+
+    """
+
+    messages: Annotated[list, add_messages]
+
+
